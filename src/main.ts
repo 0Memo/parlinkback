11414d0bef27c87636ec10866ad7a2d3f8f2194a
Guillaume Mehats <guillaume.mehats@gmail.com>
2024-12-13 20:45:17 +0100

    updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 /* eslint-disable prettier/prettier */
 import * as dotenv from 'dotenv';
 dotenv.config();
+import compression from 'compression';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import { createClient } from '@redis/client';
+import helmet from 'helmet';
+import * as redis from 'redis';
 
-async function bootstrap() {
+let redisClient: redis.RedisClientType;
+
+export default async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
@@ -21,14 +26,43 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   console.log('Global validation pipe configured.');
 
-  const redisClient = createClient({
-    url: process.env.REDIS_URL,
-    socket: {
-      connectTimeout: 10000,
-    },
+  app.use(compression());
+
+  app.use(helmet());
+  console.log('Helmet configured.');
+
+  const config = new DocumentBuilder()
+    .setTitle('alt-bootcamp')
+    .setDescription('The alt-bootcamp API description')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  console.log('Swagger documentation configured.');
+
+  app.enableShutdownHooks();
+  app.getHttpAdapter().getInstance().on('close', async () => {
+    if (redisClient.isOpen) {
+      await redisClient.disconnect();
+      console.log('Redis client disconnected successfully.');
+    }
   });
-  
-  redisClient.connect();
+
+  const getRedisClient = async () => {
+    if (!redisClient) {
+      redisClient = redis.createClient({
+        url: process.env.REDIS_URL,
+        socket: {
+          tls: true,
+          reconnectStrategy: () => 1000,
+          connectTimeout: 10000,
+        },
+      });
+      await redisClient.connect();
+    }
+    return redisClient;
+  };
 
   if (process.env.NODE_ENV === 'production') {
     console.log('Production mode detected.');
@@ -43,5 +77,3 @@ async function bootstrap() {
   await app.listen(process.env.PORT || 3000);
   console.log(`L'application écoute sur le port: ${await app.getUrl()}`);
 }
-
-export default bootstrap();
