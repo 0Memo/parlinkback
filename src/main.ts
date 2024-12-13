ba1b0c3a735f3b1d538ce2d402b317c02abfcc28
Guillaume Mehats <guillaume.mehats@gmail.com>
2024-12-13 20:37:14 +0100

    updated vercel.json & main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 /* eslint-disable prettier/prettier */
 import * as dotenv from 'dotenv';
 dotenv.config();
-import compression from 'compression';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,11 +19,6 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   console.log('Global validation pipe configured.');
-
-  app.use(compression());
-
-  app.use(helmet());
-  console.log('Helmet configured.');
 
   if (process.env.NODE_ENV === 'production') {
     console.log('Production mode detected.');
